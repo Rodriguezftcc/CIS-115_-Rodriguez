4e978ca8beb416f2d2f18aab094f2ebc4cf56435
Rodriguezftcc <55244460+Rodriguezftcc@users.noreply.github.com>
2019-09-12 10:43:41 -0400

    M2Lab_Rodriguez

---
+++ m2lab_Rodriguez.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+M2T1
+Rodriguez
+9/12/10
+"""
+
+# Covert M2T1 into Python
+
+
+#//M2LAB 
+#//Alex Rodriguez
+#9/12/19
+
+#//Greet the user by name
+#//then tell them their age
+#//on their next birthday
+
+#//Declarations
+#Declare string name
+#Declare interger age
+
+
+#Display "Hello ! What's your name?"
+print("Hello! What's your name?")
+#Input name
+name = input()
+
+#Display "How old are you?"
+print("How old are you?")
+#input age
+age = int(input())
+
+#set age = age + 1
+age = age + 1 
+
+#Display Well, ", name, " on your next birthday ..."
+#Display "You will be "' age, "!"
+print("Your name is",name)
+print("You will be",age,"on our next birthday")
